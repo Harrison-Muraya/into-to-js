3961500615a86d504d24cca27826f2f3a81ad6a0
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-01-29 13:09:04 +0300

    template strings

--- lesson-1/templateStrings/tempstr.js
+++ lesson-1/templateStrings/tempstr.js
@@ -3,4 +3,15 @@ const auther = 'harrison'
 const likes = 10
 
 // concatenate way
- 
+let results1 = 'the blog called  ' + title  + ' by ' + auther + ' has ' + likes + 'likes'
+console.log(results1)
+
+// template way
+let results = `the blog called ${title} by ${auther} has ${likes} likes`
+console.log(results)
+
+// creating html templates
+ let html = `<h2>${title}</h2>
+            <p>by ${auther},</P>
+            <span> this blog has ${likes} likes</span>`
+console.log(html)
