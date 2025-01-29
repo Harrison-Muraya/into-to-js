b8eca519636c8fff6c62ea2200fa8fcb054a5546
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-01-29 14:14:54 +0300

    loose$strict comparison

---
+++ lesson-1/loose_vs_strict_comparison/loose_vs_strict_comparison.js
@@ -0,0 +1,11 @@
+let age = 25
+
+// loose comparision
+
+console.log(age == 25) //true
+console.log(age == '25') //true
+
+// strict comparison
+console.log(age === 25) //check up to type
+console.log(age !== 25)
+console.log(age !== '25')
