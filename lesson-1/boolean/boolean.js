23b50cfde6baf6eed1e361f0b0c61409b8accb79
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-01-29 13:54:42 +0300

    boolean

---
+++ lesson-1/boolean/boolean.js
@@ -0,0 +1,26 @@
+console.log(true, false)
+
+let email = 'harrison@gnail.com'
+let ninjas = ['shaun','harr','kim' ]
+
+let results = email.includes('@') // returns true
+let results11 = ninjas.includes('harr') //true
+let results1 = email.includes('!') // returns false
+
+// comparison operator
+let age =25
+console.log(age==25)
+console.log(age==35)
+console.log(age!=25)
+console.log(age>20)
+console.log(age<25)
+console.log(age<=25)
+console.log(age>=25)
+
+let name = 'harr'
+
+console.log('strings ',name =='harr')
+console.log('strings ',name =='Harr')
+console.log('strings ',name >'mura')
+console.log('strings ',name >'Harr')
+console.log('strings ',name >'Mura')
