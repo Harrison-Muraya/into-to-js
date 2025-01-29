6bbbc410ca267214e56757b735da4962e59b533a
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-01-29 14:27:03 +0300

    type conversion

---
+++ lesson-1/type_comparison/type comparison.js
@@ -0,0 +1,13 @@
+let score = '100'
+score = Number(score)
+console.log(score + 1)
+console.log(typeof score)
+
+let result1 = String(50)
+let result2 = Number(score)
+let result3 = Boolean(0)
+let result4 = Boolean('0')
+
+console.log(result, typeof result)
+
+  
