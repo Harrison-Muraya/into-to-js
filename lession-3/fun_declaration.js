b8715f157752893fc33fd8deb618218708344a58
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-01-29 17:12:14 +0300

    function declaration

---
+++ lession-3/fun_declaration.js
@@ -0,0 +1,13 @@
+// function declaration
+function great(){
+    console.log(' am in function')
+}
+
+// function expression
+const newgreating = function(){
+    console.log('am good')
+}
+
+// calling a function
+great()
+newgreating()
