b03ea0156613bc32487f38f025439bfca7f8b5fd
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-01 23:59:46 +0300

    inner text

--- lession-6/dom.js
+++ lession-6/dom.js
@@ -1,13 +1,13 @@
 const para = document.querySelector('body > div > p')
-const par = document.querySelectorAll('p')
+const parr = document.querySelectorAll('p')
 
-console.log("this is from from SelectorAll",par)
+console.log("this is from from SelectorAll",parr)
 
-par.forEach(pa=>{
+parr.forEach(pa=>{
     console.log(pa)
 })
 
-console.log(par)
+console.log(parr)
 console.log(para)
 
 // other was of selector
@@ -17,3 +17,15 @@ const error = document.getElementsByClassName('error')
 console.log(title)
 console.log(error)
 
+// adding and changing page content
+
+const h1 = document.querySelector('h1')
+const paras = document.querySelectorAll('p')
+
+paras.forEach(para=>{
+    para.innerText += `this is new text from javascript`
+})
+
+h1.innerText = 'wellcome '
+
+console.log(h1.innerText)
