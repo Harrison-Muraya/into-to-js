85965a339f11c1186ea73296ff7d0a251081a911
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-01 03:24:56 +0300

    adding methods

--- lession-5/object_literal.js
+++ lession-5/object_literal.js
@@ -2,6 +2,7 @@ const user = {
     name: 'harrison',
     age: 28,
     email: 'harrisonmuraya@gmail.com',
+    location: 'nairobi',
     blog: ['how to write java', 'c# is the best', 'c# is the best'],
 }
 
@@ -22,3 +23,24 @@ user.name = 'kimani'
 console.log(user.name)
 console.log(user.blog[1])
 console.log(typeof user)
+
+
+
+const person = {
+    name: 'harrison',
+    age: 28,
+    email: 'harrisonmuraya@gmail.com',
+    location: 'nairobi',
+    blog: ['how to write java', 'c# is the best', 'c# is the best'],
+    login: function(){
+        console.log('you are logged in')
+    },
+
+    logout: function(){
+        console.log('you are logged out now')
+    }
+}
+
+person.login()
+person.logout()
+
