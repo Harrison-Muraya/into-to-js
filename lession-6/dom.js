e0ed9cdeb96f9bf08c6fcab3f2284a298f2d4496
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-01 23:05:28 +0300

    query selector

---
+++ lession-6/dom.js
@@ -0,0 +1,11 @@
+const para = document.querySelector('body > div > p')
+const par = document.querySelectorAll('p')
+
+console.log("this is from from SelectorAll",par)
+
+par.forEach(pa=>{
+    console.log(pa)
+})
+
+console.log(par)
+console.log(para)
