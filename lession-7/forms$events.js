e6417e57b9eb3934cc7bbd34197f2981f9db2247
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-04 23:43:25 +0300

    testing dada and validating

--- lession-7/forms$events.js
+++ lession-7/forms$events.js
@@ -7,4 +7,16 @@ form.addEventListener('submit', e=>{
     console.log(form.username.value)
 })
 
+const username = 'harrrr'
+
 // validating data form the user
+const patten = /[a-z]{6,}/
+let results = patten.test(username)
+console.log(results)
+
+if(results){
+    console.log('test passed')
+}
+else{
+    console.log('test failed')
+}
